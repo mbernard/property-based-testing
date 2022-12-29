bda0c79df0ad3443f9dee41f7c4bc70ca4696d3a
rocketOC <11656159+rocketOC@users.noreply.github.com>
2022-12-28 21:50:53 -0500

    leading padding shrinks test (#1)

--- src/Samples/Diamond.cs
+++ src/Samples/Diamond.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Samples
@@ -33,6 +34,27 @@ namespace Samples
                 yield return " A ";
                 yield return $"{c} {c}";
                 yield return " A ";
+            }
+        }
+
+        private static IEnumerable<string> Generate4(char c)
+        {
+            var length = (c - 'A' + 1) * 2 - 1;
+            var edge = "{0}" + new string(' ', Math.Max(length - 2, 0)) + "{0}";
+            var lastIndex = length - 1;
+            var aPadding = new string(' ', (length - 1) / 2);
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i == 0 || i == lastIndex)
+                {
+                    yield return  aPadding + 'A' + aPadding;
+                }
+                else
+                {
+                    char x = (char)((i <= length / 2) ? ('A' + i) : (c - (i - length/2)));
+                    yield return string.Format(edge, x);
+                }
             }
         }
 
